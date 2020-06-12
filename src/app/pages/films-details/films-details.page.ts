60f7db1b2eec76aa56be6f982f5cf1ad774b1765
Ary Sugiarto <arysugiarto10@gmail.com>
2020-06-12 20:53:35 +0700

    initial 1

---
+++ src/app/pages/films-details/films-details.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+ 
+@Component({
+  selector: 'app-films-details',
+  templateUrl: './films-details.page.html',
+  styleUrls: ['./films-details.page.scss'],
+})
+export class FilmsDetailsPage implements OnInit {
+ 
+  filmId = null;
+ 
+  constructor(private activatedRoute: ActivatedRoute) { }
+ 
+  ngOnInit() {
+    this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+ 
+}
---
+++ src/app/pages/films/films.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-films',
+  templateUrl: './films.page.html',
+  styleUrls: ['./films.page.scss'],
+})
+export class FilmsPage implements OnInit {
+
+  constructor(private navController: NavController, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  openDetails() {
+    this.router.navigateByUrl('/tabs/films/42');
+  }
+
+  goToPlanets() {
+    this.navController.navigateRoot('/tabs/planets');
+  }
+
+}
